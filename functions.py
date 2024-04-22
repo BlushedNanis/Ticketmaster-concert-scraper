6eccd392b594ee33d355c24cef2bfa0b7aae3d67
BlushedNanis <luis_danieltg@outlook.com>
2024-04-21 23:08:43 -0400

    added functions to scrap and extract the events

--- functions.py
+++ functions.py
@@ -1,12 +1,28 @@
 from requests import get
-from selectorlib import Extractor
+from lxml.html import fromstring
 
 
-def scraper(band):
-    url = f"https://www.ticketmaster.ca/search?q={band}"
+def scrap(artist:str):
+    url = f"https://www.ticketmaster.ca/search?q={artist}"
     response = get(url)
-    source = response.text
+    source = response.content
     return source
 
-def extract(source):
-    extractor = Extractor.from_yaml_file("")
+def extract(source:bytes):
+    events_data = []
+    xpath_index = 1
+    webpage = fromstring(source)
+    while True:
+        try:
+            event = webpage.xpath(f'//*[@id="pageInfo"]/div[2]/div[1]/div[2]/div/ul/li[{str(xpath_index)}]/div[1]/div/div[3]/a/span/span/span[2]/text()')[0]
+            date = webpage.xpath(f'//*[@id="pageInfo"]/div[2]/div[1]/div[2]/div/ul/li[{str(xpath_index)}]/div[1]/div/div[3]/a/span/span/span[2]/span/text()')[0]
+            link = webpage.xpath(f'//*[@id="pageInfo"]/div[2]/div[1]/div[2]/div/ul/li[{str(xpath_index)}]/div[1]/div/div[3]/a/@href')[0]
+            events_data.append((event, date, link))
+            xpath_index += 1
+        except IndexError:
+            break
+    artist_link = webpage.xpath('//*[@id="main-content"]/div/div[1]/div[2]/div/ul/li/a/@href')[0]
+    return events_data, artist_link
+    
+
+print(extract(scrap("nothing but thieves")))
